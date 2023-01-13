7fd263b74e5b1dbe5940743ae1407b89f8df76c6
SofianeLasri <alasri250@gmail.com>
2023-01-13 21:23:00 +0100

    Vitrine bureau terminée.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,14 +14,18 @@
     <x-navbar/>
 
     {{--Vitrine--}}
-    <div class="home-showcase">
+    <div class="home-showcase" id="homeShowcase">
         <div class="projects-presentations">
             <div class="desktop-controls">
                 <div class="left">
-                    <button class="control-button" type="button" title="{{ __('word.previous') }}"><i class="fa-solid fa-chevron-left"></i></button>
+                    <button onclick="previousPresentation()" class="control-button" type="button"
+                            title="{{ __('word.previous') }}"><i
+                            class="fa-solid fa-chevron-left"></i></button>
                 </div>
                 <div class="right">
-                    <button class="control-button" type="button" title="{{ __('word.next') }}"><i class="fa-solid fa-chevron-right"></i></button>
+                    <button onclick="nextPresentation()" class="control-button" type="button"
+                            title="{{ __('word.next') }}"><i
+                            class="fa-solid fa-chevron-right"></i></button>
                 </div>
             </div>
             <div class="presentation first active">
@@ -42,7 +46,7 @@
                     <p>Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod empor incididunt.</p>
                 </div>
             </div>
-            <div class="presentation">
+            <div class="presentation third">
                 <div class="content">
                     <div class="notice-text">
                         <span>Projet en vedette</span>
@@ -56,15 +60,21 @@
             <div class="cards">
                 <div class="card active first">
                     <div class="title">Starisland</div>
-                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod empor incididunt.</div>
+                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        empor incididunt.
+                    </div>
                 </div>
                 <div class="card second">
                     <div class="title">Rosewood</div>
-                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod empor incididunt.</div>
+                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        empor incididunt.
+                    </div>
                 </div>
                 <div class="card third">
                     <div class="title">Maisonette 9</div>
-                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod empor incididunt.</div>
+                    <div class="description">Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        empor incididunt.
+                    </div>
                 </div>
             </div>
             <div class="dots-indicators">
@@ -75,3 +85,78 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script type="text/javascript">
+        const presentationTime = 10000;
+        const presentationTransitionTime = 1000;
+        const showCase = document.getElementById('homeShowcase');
+        const presentations = showCase.getElementsByClassName('presentation');
+        const presentationsCards = showCase.getElementsByClassName('card');
+
+        function getActivePresentationIndex() {
+            for (let i = 0; i < presentations.length; i++) {
+                if (presentations[i].classList.contains('active')) {
+                    return i;
+                }
+            }
+        }
+
+        function nextPresentation() {
+            let activePresentationIndex = getActivePresentationIndex();
+            let activePresentation = presentations[activePresentationIndex];
+            let nextPresentation = presentations[activePresentationIndex + 1] || presentations[0];
+            let nextPresentationIndex = activePresentationIndex + 1 < presentations.length ? activePresentationIndex + 1 : 0;
+
+            nextPresentation.style.zIndex = 200;
+            activePresentation.style.animation = 'hideRightToLeft ' + presentationTransitionTime + 'ms ease-in-out';
+            nextPresentation.style.animation = 'showLeftToRight ' + presentationTransitionTime + 'ms ease-in-out';
+            nextPresentation.classList.add('active');
+            setTimeout(() => {
+                nextPresentation.style.removeProperty('z-index');
+                nextPresentation.style.removeProperty('animation');
+                activePresentation.style.removeProperty('animation');
+                activePresentation.classList.remove('active');
+            }, presentationTransitionTime);
+
+            updatePresentationCards(nextPresentationIndex);
+        }
+
+        function previousPresentation() {
+            let activePresentationIndex = getActivePresentationIndex();
+            let activePresentation = presentations[activePresentationIndex];
+            let previousPresentation = presentations[activePresentationIndex - 1] || presentations[presentations.length - 1];
+            let previousPresentationIndex = activePresentationIndex - 1 < 0 ? presentations.length - 1 : activePresentationIndex - 1;
+
+            previousPresentation.style.zIndex = 200;
+            activePresentation.style.animation = 'hideLeftToRight ' + presentationTransitionTime + 'ms ease-in-out';
+            previousPresentation.style.animation = 'showRightToLeft ' + presentationTransitionTime + 'ms ease-in-out';
+            previousPresentation.classList.add('active');
+            setTimeout(() => {
+                previousPresentation.style.removeProperty('z-index');
+                previousPresentation.style.removeProperty('animation');
+                activePresentation.style.removeProperty('animation');
+                activePresentation.classList.remove('active');
+            }, presentationTransitionTime);
+
+            updatePresentationCards(previousPresentationIndex);
+        }
+
+        function updatePresentationCards(newPresentationIndex) {
+            let activePresentationCard = presentationsCards[newPresentationIndex];
+            let activePresentationCardDot = showCase.getElementsByClassName('dot')[newPresentationIndex];
+
+            for (let i = 0; i < presentationsCards.length; i++) {
+                presentationsCards[i].classList.remove('active');
+            }
+            for (let i = 0; i < showCase.getElementsByClassName('dot').length; i++) {
+                showCase.getElementsByClassName('dot')[i].classList.remove('active');
+            }
+
+            activePresentationCard.classList.add('active');
+            activePresentationCardDot.classList.add('active');
+        }
+
+        setInterval(nextPresentation, presentationTime);
+    </script>
+@endpush
