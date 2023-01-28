360483194a42482b822d9b29c057162b2c5746d2
Sofiane Lasri <alasri250@gmail.com>
2023-01-28 17:36:37 +0100

    Contenu de la page projet terminé.

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -71,43 +71,90 @@
                                 <div class="linkOverlay black"></div>
                             </div>
                         @endfor
-                            <div class="bg-light aspect-video h-32 flex items-center justify-center">
-                                <div class="h-6">
-                                    <x-logo-short color="#000"/>
-                                </div>
+                        <div class="bg-light aspect-video h-32 flex items-center justify-center">
+                            <div class="h-6">
+                                <x-logo-short color="#000"/>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="custom-container">
-        <h1>Présentation</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut faucibus pulvinar elementum integer enim neque. Adipiscing elit ut aliquam purus sit amet.
-            Potenti nullam ac tortor vitae. Amet consectetur adipiscing elit ut aliquam purus sit. Scelerisque viverra
-            mauris in aliquam sem fringilla ut morbi tincidunt. Ipsum dolor sit amet consectetur adipiscing elit duis
-            tristique sollicitudin. Diam quis enim lobortis scelerisque. Mattis rhoncus urna neque viverra justo nec.
-            Odio morbi quis commodo odio aenean sed adipiscing diam. Tortor at risus viverra adipiscing.</p>
+    <div class="custom-container px-8 py-10 flex flex-col lg:flex-row gap-2">
+        <div>
+            <h1>Présentation</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore
+                magna aliqua. Ut faucibus pulvinar elementum integer enim neque. Adipiscing elit ut aliquam purus sit
+                amet.
+                Potenti nullam ac tortor vitae. Amet consectetur adipiscing elit ut aliquam purus sit. Scelerisque
+                viverra
+                mauris in aliquam sem fringilla ut morbi tincidunt. Ipsum dolor sit amet consectetur adipiscing elit
+                duis
+                tristique sollicitudin. Diam quis enim lobortis scelerisque. Mattis rhoncus urna neque viverra justo
+                nec.
+                Odio morbi quis commodo odio aenean sed adipiscing diam. Tortor at risus viverra adipiscing.</p>
 
-        <h2>Lorem ipsum</h2>
-        <p>Varius morbi enim nunc faucibus a pellentesque sit amet porttitor. Augue neque gravida in fermentum et
-            sollicitudin ac orci. Facilisis magna etiam tempor orci. Eget dolor morbi non arcu risus quis varius quam.
-            Aliquam vestibulum morbi blandit cursus risus. Magna sit amet purus gravida. Convallis a cras semper auctor.
-            Diam maecenas ultricies mi eget. Maecenas sed enim ut sem viverra aliquet. Tellus mauris a diam maecenas sed
-            enim ut sem viverra. Diam quis enim lobortis scelerisque fermentum dui. Mi eget mauris pharetra et ultrices.
-            Donec ac odio tempor orci dapibus ultrices in iaculis.</p>
+            <h2>Lorem ipsum</h2>
+            <p>Varius morbi enim nunc faucibus a pellentesque sit amet porttitor. Augue neque gravida in fermentum et
+                sollicitudin ac orci. Facilisis magna etiam tempor orci. Eget dolor morbi non arcu risus quis varius
+                quam.
+                Aliquam vestibulum morbi blandit cursus risus. Magna sit amet purus gravida. Convallis a cras semper
+                auctor.
+                Diam maecenas ultricies mi eget. Maecenas sed enim ut sem viverra aliquet. Tellus mauris a diam maecenas
+                sed
+                enim ut sem viverra. Diam quis enim lobortis scelerisque fermentum dui. Mi eget mauris pharetra et
+                ultrices.
+                Donec ac odio tempor orci dapibus ultrices in iaculis.</p>
 
-        <p>Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Hendrerit dolor magna eget est. In
-            egestas erat imperdiet sed euismod nisi porta lorem mollis. Fusce ut placerat orci nulla. Ultrices tincidunt
-            arcu non sodales neque sodales ut. Posuere ac ut consequat semper viverra. Egestas dui id ornare arcu odio
-            ut sem nulla pharetra. Fringilla urna porttitor rhoncus dolor. Rhoncus mattis rhoncus urna neque viverra.
-            Adipiscing bibendum est ultricies integer quis auctor. Metus vulputate eu scelerisque felis imperdiet proin
-            fermentum. Aliquam malesuada bibendum arcu vitae. Suspendisse faucibus interdum posuere lorem ipsum dolor.
-            Amet justo donec enim diam vulputate ut pharetra sit. Massa sed elementum tempus egestas sed. Pellentesque
-            elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Vulputate ut pharetra sit amet.
-            Semper viverra nam libero justo laoreet sit amet.</p>
+            <p>Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Hendrerit dolor magna eget est.
+                In
+                egestas erat imperdiet sed euismod nisi porta lorem mollis. Fusce ut placerat orci nulla. Ultrices
+                tincidunt
+                arcu non sodales neque sodales ut. Posuere ac ut consequat semper viverra. Egestas dui id ornare arcu
+                odio
+                ut sem nulla pharetra. Fringilla urna porttitor rhoncus dolor. Rhoncus mattis rhoncus urna neque
+                viverra.
+                Adipiscing bibendum est ultricies integer quis auctor. Metus vulputate eu scelerisque felis imperdiet
+                proin
+                fermentum. Aliquam malesuada bibendum arcu vitae. Suspendisse faucibus interdum posuere lorem ipsum
+                dolor.
+                Amet justo donec enim diam vulputate ut pharetra sit. Massa sed elementum tempus egestas sed.
+                Pellentesque
+                elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Vulputate ut pharetra sit amet.
+                Semper viverra nam libero justo laoreet sit amet.</p>
+        </div>
+        <div class="lg:w-80 shrink-0">
+            <div class="flex flex-col">
+                <h3><i class="fa-solid fa-clock-rotate-left"></i> Chronologie</h3>
+
+                <div class="space-y-2">
+                    <div>
+                        <strong class="line-before">Janvier 2017</strong>
+                        <ul class="list-disc ml-8">
+                            <li>Création du projet</li>
+                            <li>Recherche d'un partenaire</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <strong class="line-before">Janvier 2017</strong>
+                        <ul class="list-disc ml-8">
+                            <li>Création du projet</li>
+                            <li>Recherche d'un partenaire</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <strong class="line-before">Janvier 2017</strong>
+                        <ul class="list-disc ml-8">
+                            <li>Création du projet</li>
+                            <li>Recherche d'un partenaire</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <x-footer/>
