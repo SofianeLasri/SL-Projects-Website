145488bfc6d3e73e9545dca40282042d9247c2f6
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2023-07-25 11:47:50 +0200

    Modification feature test + test env - Lancement du test sans vite - Modification url test sofianelasri.fr - Ajout encryption key

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:refresh');
+    }
+
     /**
      * A basic test example.
      *
@@ -14,8 +19,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
-
+        $this->withoutVite();
+        $response = $this->get(route("sofianelasri.home"));
         $response->assertStatus(200);
     }
 }
