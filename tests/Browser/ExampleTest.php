581b3f9d161053dc7cbe1a55d5342ead53d4a10b
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2024-03-30 17:07:01 +0100

    debug gitlab ci

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             echo route('showcase.home');
+            echo env('SHOWCASE_DOMAIN');
             $browser->visit(route('showcase.home'))
                 ->assertSee('Sofiane Lasri');
         });
